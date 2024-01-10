04e5271a11d52d734caa5331f0c06f88365f3f3a
Ryouta32 <90999621+Ryouta32@users.noreply.github.com>
2024-01-10 15:59:49 +0900

    授業内容反映

--- RenderTexture/Assets/Scripts/SphereBehaviourScript.cs
+++ RenderTexture/Assets/Scripts/SphereBehaviourScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SphereBehaviourScript : MonoBehaviour
 {
+    [SerializeField] Transform target;
+    [SerializeField] GameObject Plane;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,8 @@ public class SphereBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Plane.transform.position = target.position+new Vector3(0,1,0);
+        //transform.position = target.position+new Vector3(0,10,0);
         transform.position = new Vector3(0.5f * Mathf.Sin(0.2f * Time.time), 0.0f, 0.8f);
     }
 }
